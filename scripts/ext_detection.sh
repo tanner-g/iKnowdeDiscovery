ab85559277f23f781d2089c18d93e2ecaf46eaa3
tanner-g <8582756+tanner-g@users.noreply.github.com>
2018-04-15 02:21:28 -0400

    forgot to change the dev variable user_path_test back to expected user_path

--- scripts/ext_detection.sh
+++ scripts/ext_detection.sh
@@ -5,4 +5,4 @@
 # Checking if the filesystem is either in ext2 or ext3/4
 echo [COMMAND] sudo file -s $filesystempath
 filesysteminfo=$(sudo file -s $filesystempath > user_path)
-python ext_detect.py < user_path_test
+python ext_detect.py < user_path
