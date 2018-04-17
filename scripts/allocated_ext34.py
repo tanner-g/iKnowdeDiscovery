2f7665153d65900ffb476c8d521a41efcc7f1233
meghan <mrg7540@rit.edu>
2018-04-17 02:04:15 -0400

    Renamed allocated.py file

---
+++ scripts/allocated_ext34.py
@@ -0,0 +1,10 @@
+if "Allocated" in open("/usr/tmp/iKnowdeDiscovery/inode_alloc_result.txt").read():
+    f = open("/usr/tmp/iKnowdeDiscovery/inode_alloc_result.txt", "r")
+    fi = open("/usr/tmp/iKnowdeDiscovery/unallocated_inodes.txt", "a")
+    for line in f:
+        if "inode:" in line:
+            inode = line.strip().split(" ")[1]
+        if "Inode Modified" in line:
+            beginning, date = line.strip().split("\t")
+            if date[0] != "0":
+                fi.write(inode + "," + date + "\n")
